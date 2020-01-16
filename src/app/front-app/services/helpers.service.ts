026f51e96f15e0da7af5dc8c3400abe9e579848a
Maciej Zaziablo <Maciej.Zaziablo@pb.com>
2020-01-16 15:22:56 +0100

    cleaned helpers service

--- src/app/front-app/services/helpers.service.ts
+++ src/app/front-app/services/helpers.service.ts
@@ -1,14 +1,11 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
 })
 export class HelpersService {
 
-    constructor(private http: HttpClient) {}
+    constructor() {}
 
     public findPageByUrl(arr, url: string | number): Object {
 
