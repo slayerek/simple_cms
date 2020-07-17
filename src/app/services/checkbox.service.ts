542ee22487298912ac1826004d1111cb1ba541ba
Maciej Zaziablo <Maciej.Zaziablo@pb.com>
2020-07-17 23:06:38 +0200

    created checkbox status component

--- src/app/services/checkbox.service.ts
+++ src/app/services/checkbox.service.ts
@@ -1,23 +1,25 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {AuthService} from './auth.service';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
-export class SortableService {
+export class CheckboxService {
 
-    private apiUrl: string = 'http://laravelapi.tescik.ovh/sortable';
+    private apiUrl: string = 'http://laravelapi.tescik.ovh/checkbox';
 
     constructor(private http: HttpClient, private authServ: AuthService) {}
 
-    updateSortItems(modelName, sortArr) {
+    updateCheckboxStatus(status,field,id,modelName) {
 
         const header = {Authorization: `Bearer ${this.authServ.getJwtToken()}`};
         let formData: any = new FormData();
         formData.append("modelName", modelName);
-        formData.append("sortArr", sortArr);
-        
+        formData.append("field", field);
+        formData.append("id", id);
+        formData.append("checkboxStatus", status);
+
         //return this.http.post(this.apiUrl, formData, {observe: 'response'});
         return this.http.post(this.apiUrl, formData, {headers: header});
 
